a70e1035a5a603fcb74a6de6f022813fcdc1b57b
JavaDev7 <xorazmwolf@gmail.com>
2024-05-19 14:39:31 +0500

    Add files via upload

---
+++ misol02.java
@@ -0,0 +1,8 @@
+public class misol02 {
+    public static void main(String[] args) {
+        int r1=3;
+        double pi=3.14;
+        double volume =r1*pi;
+        System.out.println("Doiraning yuzi:"+volume);
+    }
+}
