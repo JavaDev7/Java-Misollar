80161567824c022c523757c47285baeb4897c16c
JavaDev7 <xorazmwolf@gmail.com>
2024-05-19 14:22:31 +0500

    Add files via upload

---
+++ misol01.java
@@ -0,0 +1,8 @@
+public class misol01 {
+    public static void main(String[] args) {
+        //a soniga istagancha qiymat berish mumkin
+        int a=3;
+        double volume = a*a*a;
+        System.out.println("Hajmi:"+volume);
+    }
+}
